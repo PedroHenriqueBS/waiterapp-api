37fe883c49369a4a4a39f298ceeb4b79011f9652
Pedro Henrique <pedrohenriqueb366@gmail.com>
2026-03-03 20:42:58 -0300

    feat: configuring the API header

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ mongoose
     const app = express(); // Cria uma instância do Express para configurar o servidor HTTP
     const port = 3001;
 
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*"); // Permite que qualquer origem acesse os recursos do servidor (CORS)
+      res.header("Access-Control-Allow-Methods", "*"); // Permite que qualquer método HTTP seja utilizado nas requisições (CORS)
+      res.header("Access-Control-Allow-Headers", "*"); // Permite que qualquer cabeçalho seja utilizado nas requisições (CORS)
+      next();
+    });
+
     app.use("/uploads", express.static("uploads")); // Configura o Express para servir arquivos estáticos da pasta "uploads" quando a rota "/uploads" for acessada
     app.use(express.json()); // Permite que o Express entenda requisições com corpo em formato JSON
     app.use(router); // Registra as rotas definidas no arquivo router.ts
